bbf9ad5c749e686ae9fc97fd188ca724a96915be
Joe Badaczewski <jbadaczewski@softwriters.com>
2021-05-18 12:46:25 -0400

    sync working for inserts

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get, Query, Post, Req, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Query,
+  Post,
+  Req,
+  Body
+} from '@nestjs/common';
 import { UnitsService, UnitType } from '../unit/unit.service';
 import { SyncDatabaseChangeSet } from '@nozbe/watermelondb/sync';
 
@@ -18,15 +25,21 @@ export class AppController {
     return this.appService.getData();
   }
 
-  @Get("/sync")
+  @Get('/sync')
   async pullChanges(@Query() query: Record<string, any>) {
     const { last_pulled_at, migration, schema_version } = query;
-    const lastPulledAt = last_pulled_at === "null" ? 0 : Number(last_pulled_at);
+    const lastPulledAt =
+      last_pulled_at === 'null' ? 0 : Number(last_pulled_at);
 
-    const changes = await Object.keys(UnitType).reduce<Promise<Changes>>(async (acc, unitType) => {
+    const changes = await Object.keys(UnitType).reduce<
+      Promise<Changes>
+    >(async (acc, unitType) => {
       const a = await acc;
       a[unitType] = {
-        created: await this.unitService.getCreatedAfterTimestamp(unitType as UnitType, lastPulledAt),
+        created: await this.unitService.getCreatedAfterTimestamp(
+          unitType as UnitType,
+          lastPulledAt
+        ),
         updated: [],
         deleted: []
       };
@@ -36,31 +49,35 @@ export class AppController {
 
     delete changes.achievement;
     delete changes.checkin;
+    const now = Date.now();
 
     return {
       changes,
-      timestamp: lastPulledAt === 0 ? Date.now() : lastPulledAt
+      timestamp: now
     };
   }
 
-  @Post("/sync")
-  pushChanges(@Body() changes: SyncDatabaseChangeSet, @Query() query: Record<string, any>) {
+  @Post('/sync')
+  pushChanges(
+    @Body() changes: SyncDatabaseChangeSet,
+    @Query() query: Record<string, any>
+  ) {
     const { last_pulled_at } = query;
     for (const table in changes) {
-      for(const changeType in changes[table]) {
-        switch(changeType){
+      for (const changeType in changes[table]) {
+        switch (changeType) {
           case 'created':
             // TODO change this to a createMany
-            changes[table][changeType].forEach(unit => {
+            changes[table][changeType].forEach((unit) => {
               delete unit._status;
               delete unit._changed;
-              unit._id = unit.id
+              unit._id = unit.id;
               delete unit.id;
-              this.unitService.create({type: table, ...unit})
-            })
+              this.unitService.create({ type: table, ...unit });
+            });
         }
       }
     }
-    return "sync endpoint";
+    return 'sync endpoint';
   }
 }
